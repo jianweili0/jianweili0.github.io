458f6f745408f270551c9d345bb6bbb97d583e02
jianweili0 <lijianweidesign@gmail.com>
2018-01-25 11:27:10 -0500

    W1 Assignment
    Updated for Week 1 Assignment

--- W1/sketch2.js
+++ W1/sketch2.js
@@ -4,15 +4,21 @@ function setup() {
 
 function draw() {
 	if (mouseIsPressed){
-		var a = random(200,255)
-		var b = random(200,255)
-		var c = random(0,100)
-		var d = random(193,255)
+		var a = random(200,255);
+		var b = random(200,255);
+		var c = random(0,100);
+		var d = random(193,255);
 		fill(a,b,c,d);
-		rect(mouseX,mouseY,80,80,5)
+		rect(mouseX-40,mouseY-40,80,80,5);
+		push();// Start a new drawing state
+		
+		textSize(12);
+		fill(0);
+		text('Jianwei is here',mouseX-40,mouseY);
+		pop();// Restore original state(scale, specifically)
 		
 	}else{
-		fill(255,255,255,25)
+		fill(255,255,255,25);
 		ellipse(mouseX,mouseY,80,80);
 	}
 	
